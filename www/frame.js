cbb9107177587a4d45e692ed740c70e010b259fc
Friedrich Decker <friedrich_decker@outlook.com>
2020-04-15 17:31:29 +0200

    debugging img loaded

--- www/frame.js
+++ www/frame.js
@@ -349,6 +349,7 @@ export function onPageLoad() {
         }
     
         if (img.complete) {
+            console.log("img complete");
             loaded();
         } else {
             img.addEventListener('load', loaded);
